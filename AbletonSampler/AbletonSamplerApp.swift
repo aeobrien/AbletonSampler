63838548e1481e185c9c56516002b62d95db8eb3
Aidan O'Brien <aidan.e.obrien@gmail.com>
2025-04-20 18:50:31 +0100

    Initial Commit

---
+++ AbletonSampler/AbletonSamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbletonSamplerApp.swift
+//  AbletonSampler
+//
+//  Created by Aidan O'Brien on 20/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct AbletonSamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
